612e84903297bcaea2b497e62cae4856991fc03e
leonardosmleo <leonardosmleo@gmail.com>
2020-05-18 21:29:42 -0300

    atualizando fork

--- src/main/java/br/com/munif/cursos/treinamento/TreinamentounicesumarApplication.java
+++ src/main/java/br/com/munif/cursos/treinamento/TreinamentounicesumarApplication.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.munif.cursos.treinamento.entidades.Cidade;
+import br.com.munif.cursos.treinamento.entidades.Estado;
 import br.com.munif.cursos.treinamento.entidades.Pais;
+import br.com.munif.cursos.treinamento.repositorios.CidadeRepository;
+import br.com.munif.cursos.treinamento.repositorios.EstadoRepository;
 import br.com.munif.cursos.treinamento.repositorios.PaisRepository;
 
 @SpringBootApplication
@@ -16,6 +20,13 @@ public class TreinamentounicesumarApplication {
 	@Autowired
 	private PaisRepository paisRepository;
 
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TreinamentounicesumarApplication.class, args);
 	}
@@ -27,8 +38,13 @@ public class TreinamentounicesumarApplication {
 		Pais br=paisRepository.save(new Pais("Brasil"));
 		Pais pr=paisRepository.save(new Pais("Paraguai"));
 		
+		Estado brPr = estadoRepository.save(new Estado("Paraná", br));
+		Estado brSp = estadoRepository.save(new Estado("São Paulo", br));
 		
-		
-	}
+		Estado prAm = estadoRepository.save(new Estado("Amambai", pr));
+		  
+		Cidade cidade = cidadeRepository.save(new Cidade("Maringa", brPr)  );
+		 
+	} 
 	
 }
